47fb5f61dfcd17ff857ae1e3a2d9d6d7202e8702
ddodxy <alfariziridha014@gmail.com>
2022-02-15 12:05:46 +0800

    .

--- HachiBot/utils/paste.py
+++ HachiBot/utils/paste.py
@@ -20,3 +20,29 @@ async def paste(content):
     loop = get_running_loop()
     link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
     return link
+
+async def pastetext(text_to_print, pastetype=None, extension=None):
+    response = {"error": "something went wrong"}
+    if pastetype is not None:
+        if pastetype == "p":
+            response = await p_paste(text_to_print, extension)
+        elif pastetype == "s" and extension:
+            response = await s_paste(text_to_print, extension)
+        elif pastetype == "s":
+            response = await s_paste(text_to_print)
+        elif pastetype == "d":
+            response = await d_paste(text_to_print, extension)
+        elif pastetype == "n":
+            response = await n_paste(text_to_print, extension)
+    if "error" in response:
+        response = await p_paste(text_to_print, extension)
+    if "error" in response:
+        response = await n_paste(text_to_print, extension)
+    if "error" in response:
+        if extension:
+            response = await s_paste(text_to_print, extension)
+        else:
+            response = await s_paste(text_to_print)
+    if "error" in response:
+        response = await d_paste(text_to_print, extension)
+    return response
